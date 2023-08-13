8549ab502689bf967ac820dad7bfb64d97cfd1b3
koguls <kogulkogul4@gmail.com>
2023-08-13 17:21:34 +0530

    my first added 2 py file

---
+++ assign1.py
@@ -0,0 +1,7 @@
+#print the message
+
+name ='anand'
+day=15
+year=2021
+print("Dear "+ name +'')
+print("you have "+str(day)+" days of leave blance for this " + "\nyear "+'('+ str(year)+')')
---
+++ first.py
@@ -0,0 +1,13 @@
+name =input("enter name").upper()
+name1 =name
+print(name1)
+name ="akash"
+print(name)
+old=input("enter the name :")
+old1=name
+#for i in range :
+if(old and old1):
+    print("thank you! for register" + old)
+    print("welcome back sir :)")
+    print(name.upper())
+
